c1dee783e559103a367f8c46bfb5fd20418ebdb6
Nicolas Dufreche <n.dufreche@gmail.com>
2014-12-20 15:37:04 +0100

    debuging grabNode selector

--- src/tentacle/tentacle.js
+++ src/tentacle/tentacle.js
@@ -10,13 +10,12 @@ define('tentacle/tentacle', ['tentacle/navigator!MutationObserver'], function (M
             if ('string' !== typeof selector) {
                 throw 'tentacle need a dom identifier to work.';
             }
-            console.log(selector);
             this.base = document.querySelector(selector);
             this.observer = new MutationObserver(this.onMutated.bind(this));
         },
 
-        grabReactiveNode: function (element) {
-            var selector = '[data-tentacle]:not[data-grabed="true"]';
+        grabNode: function (element) {
+            var selector = '[data-tentacle]:not([data-grabed="true"])';
             if (element) {
                 return element.querySelectorAll(selector);
             }
@@ -26,7 +25,7 @@ define('tentacle/tentacle', ['tentacle/navigator!MutationObserver'], function (M
         onMutated: function (mutations) {
             var self = this;
             mutations.forEach(function (mutation) {
-                var nodes = self.grabReactiveNode(mutation.target);
+                var nodes = self.grabNode(mutation.target);
                 console.log(nodes);
             });
         }
